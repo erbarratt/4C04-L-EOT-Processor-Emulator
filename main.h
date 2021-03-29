8cae70f0145f934f16b7f2b7d9860375f7acb878
Elliott <elliott.barratt@tabx.online>
2021-03-29 09:31:55 +0100

    first commit

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef INC_4C04_MAIN_H
+	#define INC_4C04_MAIN_H
+#endif //INC_4C04_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint-gcc.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "gfx.h"
