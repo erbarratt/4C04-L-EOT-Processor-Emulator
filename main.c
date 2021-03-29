d76a1344a15ea77d58c4cda168b17b99bfcdc79c
Elliott <elbazuk@gmail.com>
2021-03-29 20:36:46 +0100

    x11 install

--- main.c
+++ main.c
@@ -1,5 +1,41 @@
 #include "main.h"
+#include <X11/Xlib.h>
+/*
+A simple example of using the gfx library.
+CSE 20211
+9/7/2011
+by Prof. Thain
+*/
 
-int main(){
-	return 0;
+int main(void) {
+   Display *d;
+   Window w;
+   XEvent e;
+   const char *msg = "Hello, World!";
+   int s;
+ 
+   d = XOpenDisplay(NULL);
+   if (d == NULL) {
+      fprintf(stderr, "Cannot open display\n");
+      exit(1);
+   }
+ 
+   s = DefaultScreen(d);
+   w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
+                           BlackPixel(d, s), WhitePixel(d, s));
+   XSelectInput(d, w, ExposureMask | KeyPressMask);
+   XMapWindow(d, w);
+ 
+   while (1) {
+      XNextEvent(d, &e);
+      if (e.type == Expose) {
+         XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
+         XDrawString(d, w, DefaultGC(d, s), 10, 50, msg, (int)strlen(msg));
+      }
+      if (e.type == KeyPress)
+         break;
+   }
+ 
+   XCloseDisplay(d);
+   return 0;
 }
